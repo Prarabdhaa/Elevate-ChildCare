b9e67fd9d077192d7bef5265483d22309255709a
Kevin Huang <khuan042@uottawa.ca>
2018-09-22 22:15:38 -0400

    edited file

--- js/main.js
+++ js/main.js
@@ -241,6 +241,7 @@
     } , { offset: '95%' } );
 
   }
+
   counter();
 
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
@@ -256,3 +257,12 @@
 
 
 })(jQuery);
+
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
